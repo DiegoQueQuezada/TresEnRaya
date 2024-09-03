b8961d1a6f02e989082e7a7e679a70b3ac199716
Diego Quezada <diegodavid0404@hotmail.com>
2024-09-02 19:25:58 -0500

    Agrege otro print final

--- src/Formas/TresEnRaya2.java
+++ src/Formas/TresEnRaya2.java
@@ -19,6 +19,7 @@ public class TresEnRaya2 extends javax.swing.JFrame {
         System.out.println("Seteando titulo 2");
         System.out.println("Seteando titulo 3");
         System.out.println("Seteando titulo 4");
+        System.out.println("Seteando ultimo");
         super.setTitle(title); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
     }
 
